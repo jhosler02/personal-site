a02533d509d4001603ab6e16bc37808341f92510
Jack H <jhosler02@gmail.com>
2025-11-08 03:40:41 -0500

    stuff

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": [],
+        "tags": ["test2"],
+        "url": "/2025/01/15/wopy-3.html",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": [],
+        "tags": ["test"],
+        "url": "/2025/01/15/welcome-to-jekyll-copy-2.html",
+        "teaser": null
+      }]
